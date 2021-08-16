d97d75d55a87c988790d310ae62de62199bba123
MohFahmi27 <mohammadfahmi417@gmail.com>
2021-08-16 23:03:43 +0800

    UPDATE: add template to main app.

--- django_app/main/apps.py
+++ django_app/main/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class MyFirstDjangoAppConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'my_first_django_app'
+    name = 'main'
--- django_app/main/views.py
+++ django_app/main/views.py
@@ -1,8 +1,31 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-def home(request) -> HttpResponse :
-    return HttpResponse('<h1>Something Good</h1>')
-    
-def about(request) :
-    return HttpResponse('<h2>Mohammad Fahmi</h2>')
+dummyPost = [
+    {
+        'author': 'Someone',
+        'title': 'Blog Post 1',
+        'content': 'Something good happen in the end of the tunnel',
+        'date_posted': '16 August 2021'
+    },
+    {
+        'author': 'MachineLearning12',
+        'title': 'Blog Post 2',
+        'content': 'OpenAI will always going to be much better over the time',
+        'date_posted': '15 August 2021'
+    },
+    {
+        'author': 'Mohammad Fahmi',
+        'title': 'Blog Post 3',
+        'content': 'There will always be something good happen',
+        'date_posted': '17 August 2021'
+    }
+]
+
+
+def home(request) -> HttpResponse:
+    return render( request, template_name='main/home.html', context={'posts': dummyPost})
+
+
+def about(request) -> HttpResponse:
+    return render(request, template_name='main/about.html', context={'title': 'About'})
