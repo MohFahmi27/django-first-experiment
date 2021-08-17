5c595ba3f2a42d81b7cb004ad8d02d55f08f332c
MohFahmi27 <mohammadfahmi417@gmail.com>
2021-08-18 00:13:27 +0800

    UPDATE: add Post database and user auth

--- django_app/main/views.py
+++ django_app/main/views.py
@@ -1,30 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
-dummyPost = [
-    {
-        'author': 'Someone',
-        'title': 'Blog Post 1',
-        'content': 'Something good happen in the end of the tunnel',
-        'date_posted': '16 August 2021'
-    },
-    {
-        'author': 'MachineLearning12',
-        'title': 'Blog Post 2',
-        'content': 'OpenAI will always going to be much better over the time',
-        'date_posted': '15 August 2021'
-    },
-    {
-        'author': 'Mohammad Fahmi',
-        'title': 'Blog Post 3',
-        'content': 'There will always be something good happen',
-        'date_posted': '17 August 2021'
-    }
-]
+from .models import Post
 
 
 def home(request) -> HttpResponse:
-    return render( request, template_name='main/home.html', context={'posts': dummyPost})
+    return render(request, template_name='main/home.html', context={'posts': Post.objects.all()})
 
 
 def about(request) -> HttpResponse:
