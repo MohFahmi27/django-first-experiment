e009a4a566c4fd41741e22d6a632839bd9982b4d
MohFahmi27 <mohammadfahmi417@gmail.com>
2021-08-19 00:00:58 +0800

    UPDATE: add register user feature.

--- django_app/django_app/settings.py
+++ django_app/django_app/settings.py
@@ -32,6 +32,8 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'main.apps.MyFirstDjangoAppConfig',
+    'users.apps.UsersConfig',
+    'bulma',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
