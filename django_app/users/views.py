0b17815839d742689a990b7af54933f764ea82d1
MohFahmi27 <mohammadfahmi417@gmail.com>
2021-08-19 21:36:15 +0800

    UPDATE: Add login and logout feature.

--- django_app/users/views.py
+++ django_app/users/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 from .forms import UserRegisterForm
 
 
@@ -10,8 +11,14 @@ def register(request) -> HttpResponse:
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get('username')
-            messages.success(request, f'Account created for {username}!')
-            return redirect('main-home')
+            messages.success(
+                request, f'Account created for {username}, Go ahead and Log In!')
+            return redirect('login')
     else:
         form = UserRegisterForm()
     return render(request, template_name='users/register.html', context={'form': form})
+
+
+@login_required
+def profile(request) -> HttpResponse:
+    return render(request, template_name='users/profile.html', context={'title': 'Profile'})
