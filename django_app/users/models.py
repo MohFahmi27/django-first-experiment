a23a3760d079d6fb0d362aea2be62341e3e8c119
MohFahmi27 <mohammadfahmi417@gmail.com>
2021-08-21 00:13:17 +0800

    UPDATE: feature update profile.

--- django_app/users/models.py
+++ django_app/users/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
-
+from PIL import Image
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
@@ -8,3 +8,12 @@ class Profile(models.Model):
 
     def __str__(self) -> str:
         return f'{self.user.username} Profile'
+
+    def save(self, *args, **kwargs) -> None:
+        super().save(*args, **kwargs)
+
+        img = Image.open(self.image.path)
+
+        if img.height > 300 or img.width > 300:
+            img.thumbnail(size=(300, 300))
+            img.save(self.image.path)
--- django_app/users/views.py
+++ django_app/users/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
-from .forms import UserRegisterForm
+from .forms import UserRegisterForm, UserUpdateForm, ProfileUpdateForm
 
 
 def register(request) -> HttpResponse:
@@ -21,4 +21,23 @@ def register(request) -> HttpResponse:
 
 @login_required
 def profile(request) -> HttpResponse:
-    return render(request, template_name='users/profile.html', context={'title': 'Profile'})
+    if request.method == 'POST':
+        user_form = UserUpdateForm(request.POST, instance=request.user)
+        profile_form = ProfileUpdateForm(
+            request.POST, request.FILES, instance=request.user.profile)
+        if user_form.is_valid() and profile_form.is_valid():
+            user_form.save()
+            profile_form.save()
+            messages.success(
+                request, f'Account have been updated!')
+            return redirect('user-profile')
+    else:
+        user_form = UserUpdateForm(instance=request.user)
+        profile_form = ProfileUpdateForm(instance=request.user.profile)
+
+    return render(request, template_name='users/profile.html',
+                  context={
+                      'title': 'Profile',
+                      'user_form': user_form,
+                      'profile_form': profile_form
+                  })
